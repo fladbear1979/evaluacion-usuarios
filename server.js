4d16234b7cbe0103c8f2ff5b5ea9a6438791bcf9
fladbear1979 <sfy6p390s1eh@hotmail.com>
2025-09-06 13:56:02 -0500

    Update log message to use 'Server' instead of 'Servidor' for consistency.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
